71fc44923797971c9f52c22d70d522dcf5796b93
Andrei-David Nan <nandavid190@gmail.com>
2024-04-15 15:05:31 +0300

    v0.4 - add prisma init migrations, update docker-compose posgres image;

---
+++ src/auth/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '.prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const dbUrl = process.env.DATABASE_URL;
+
+    super({
+      datasources: { db: { dbUrl } },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async cleanDb() {
+    if (process.env.NODE_ENV === 'prod') return;
+
+    return Promise.all([this.user.deleteMany()]);
+  }
+}
